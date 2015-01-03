6a177fb141d9d030619fc4467195777cf296128d
Stefano Dacchille <stefano.dacchille@gmail.com>
2015-01-03 15:42:04 +0100

    Add parsing of search query

--- Server.hs
+++ Server.hs
@@ -9,21 +9,24 @@ import Data.Default (def)
 import Network.Wreq hiding (headers)
 import Control.Lens
 import Data.Aeson.Lens
-import Data.Text hiding (foldr)
+import Data.Text hiding (foldr, filter, head, tail, reverse, map)
 import Data.Text.Encoding
 
 buildJson xs = dropWhileEnd (==',') $ foldr (\e json -> append (append (append (pack "\"") e) (pack "\",")) json) empty xs
 buildResponseBody r = append (append (pack "{\"results\":{\"previewUrls\":[") (buildJson $ extractPreviewUrls r)) (pack "]}}")
 extractPreviewUrls r = r ^.. responseBody . key "tracks" . key "items" . values . key "preview_url" . _String
+getQueryParams (Env _ _ _ queryString _ _ _ _ _ _ _ _) =
+                              let queryArray = map (splitOn "=") (splitOn "&" (decodeUtf8 queryString))
+                              in [ (a, b) | arr <- queryArray, a <- head arr : [], b <- tail arr]
+getQueryParamValue p env = snd . head $ filter (\(a, b) -> a == p) (getQueryParams env)
        
 app :: Application
 app = \env ->
-  let opts = defaults & param "q"     .~ ["Spit"]
+  let opts = defaults & param "q"     .~ [ getQueryParamValue "q" env ]
                       & param "type"  .~ ["track"]
                       & param "limit" .~ ["5"]
   in
     do r <- getWith opts "https://api.spotify.com/v1/search"
-       --putStrLn (unpack $ r ^. responseBody . _String)
        return $ set_body_bytestring (encodeUtf8 $ buildResponseBody r) (def { headers = [ ("Content-Type", "text/json") ] })
 
 main = run app
