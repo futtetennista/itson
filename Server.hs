6e8fb627b4e58bb0bf1c223a8ecf8ef31f21e933
Stefano Dacchille <stefano.dacchille@gmail.com>
2015-02-01 00:05:38 +0100

    First working version! Following the hacker way, I removed generalisation so it works just with spotify for now.

--- Server.hs
+++ Server.hs
@@ -10,34 +10,34 @@ import Network.Wreq as Wreq hiding (headers)
 import Network.HTTP.Types.URI (decodePathSegments)
 import Control.Lens
 import Data.Aeson.Lens
-import qualified Data.Aeson.Types as AT (Value(String))
-import qualified Data.Text as T (dropWhileEnd, append, empty, splitOn, unpack, pack, intercalate)
+import qualified Data.Aeson.Types as AT (Value(String), emptyArray)
+import qualified Data.Text as T (Text(..), dropWhileEnd, append, empty, splitOn, unpack, pack, intercalate)
 import Data.Text.Encoding (encodeUtf8)
 import qualified Data.ByteString.Char8 as C8 (pack, unpack, split)
 import Data.Monoid
-import qualified Data.ByteString as BS (empty)
+import qualified Data.ByteString as BS (ByteString(..), empty, unpack)
 import Data.ByteString.Builder
-import Data.ByteString.Lazy as BSL (toStrict)
+import qualified Data.ByteString.Lazy as BSL (toStrict)
 import qualified Data.Vector as V (filter, null, empty)
 import qualified Data.Aeson as A
 import Data.Char (toLower)
-import Data.String
 
+-- Models
 data Service = Spotify deriving Show
 dataProviders = [Spotify]
 
--- Models
 data Urls  = U [(String, String)] deriving Show
 data Title = TT String deriving Show
-data Item = I Title Urls deriving Show
+data Item = I Title Urls | Empty deriving Show
 data ServiceData = SD { name  :: String
                       , items :: [Item]
                       } deriving Show
 data Body = B [ServiceData] deriving Show
 
 instance A.ToJSON Item where
-         toJSON (I (TT title) (U [(k, v)])) = A.object ["title" A..= title, (T.pack k) A..= v]
+         toJSON (I (TT title) (U kvs)) = A.object $ ("title" A..= title) : ["urls" A..= A.object (map (\(k, v) -> T.pack k A..= v) kvs)]
 instance A.ToJSON ServiceData where
+         toJSON (SD name [])    = A.object ["service" A..= name, "items" A..= AT.emptyArray]
          toJSON (SD name items) = A.object ["service" A..= name, "items" A..= items]
 instance A.ToJSON Body where
          toJSON (B body) = A.object ["results" A..= body]
@@ -48,59 +48,34 @@ intercalateM sep xs = T.intercalate sep . filter (/="") $ map (\ m -> case m of 
 createTitle (Just t) (a:as) = TT $ (T.unpack t) ++ " by " ++ (T.unpack $ intercalateM ", " (a:as))
 createTitle (Just t) []     = TT (T.unpack t)
 createTitle Nothing  _      = TT "Song Details Unknown"
---createUrls :: IsString s => Maybe s -> Maybe s -> Urls
+--createUrls :: Data.String.IsString s => Maybe s -> Maybe s -> Urls
 createUrls (Just p) (Just f) = U [("preview", T.unpack p), ("full", T.unpack f)]
 createUrls Nothing  (Just f) = U [("full", T.unpack f)]
 createUrls _ _               = U []
-          
--- buildItem r = I (T $ createTitle (parseTitles r, parseArtists r)) (URLS $ parsePreviewUrls r)
--- buildServiceData :: String ->  -> ServiceData
-buildServiceData service datas =
-                 SD { name  = map toLower $ show service
-                    , items = datas
-                    }
 
-prepareSpotifyRequest query =
-                   (opts, "https://api.spotify.com/v1/search")
-                   where
-                        opts = defaults & param "q"     .~ [query]
-                                        & param "type"  .~ ["track"]
-                                        & param "limit" .~ ["5"]
---getServiceData :: (Service, String) -> ServiceData
-getServiceData (service, query) =
-               let
-                  urlComponents = case service of
-                                       Spotify -> prepareSpotifyRequest query
-               in do
-                  r       <- getWith (fst urlComponents) (snd urlComponents)
-                  results <- parseData r
-                  return $ buildServiceData service results
-                  
---search :: String -> [ServiceData]
-search query = return $ map getServiceData $ zip dataProviders [query]
-buildBody :: [ServiceData] -> Body
-buildBody datas = B datas
--- bodyToJSON :: Body b, Data.ByteString bs => b -> bs
-bodyToJSON b = A.encode b
--- bodyToString $ buildBody (buildServiceData "Spotify" r)
-
+createSpotifyRequest query =
+                     (opts, "https://api.spotify.com/v1/search")
+                     where
+                          opts = defaults & param "q"     .~ [query]
+                                          & param "type"  .~ ["track"]
+                                          & param "limit" .~ ["5"]
 -- Parsing
 parseAvailableMarkets r =
                       let mx = r ^.. responseBody . key "tracks" . key "items" . values . key "available_markets" . _Array
                       in case mx of
                               [] -> V.empty
                               xs -> head mx
+parseData :: (AsValue body) => Service -> Wreq.Response body -> [Item]
+parseData service resp = resp ^.. responseBody . key "tracks" . key "items" . _Array . traverse . to (\o -> I (createTitle (o ^? key "name" . _String) (o ^.. key "artists" . _Array . traverse . to (\a -> a ^? key "name" . _String))) (createUrls (o ^? key "preview_url" . _String) (o ^? key "external_urls" . key "spotify" . _String)))
 
-parseData r = return $ r ^.. responseBody . key "tracks" . key "items" . _Array . traverse . to (\o -> I (createTitle (o ^? key "name" . _String) (o ^.. key "artists" . _Array . traverse . to (\a -> a ^? key "name" . _String))) (createUrls (o ^? key "preview_url" . _String) (o ^? key "external_urls" . key "spotify" . _String)))
-
--- Request
+-- Incoming Request
 getQueryParams env =
                let queryArray = map (T.splitOn "=") $ T.splitOn "&" (head $ decodePathSegments (H2.queryString env))
                in [ (a, b) | arr <- queryArray, a <- [head arr], b <- tail arr]
 getQueryParamValue p env = snd . head $ filter (\(a, b) -> a == p) (getQueryParams env)
 
 printableRequestHeaders headers = foldr (\(k, v) hx -> mappend (mappend hx $ mappend (byteString k) (mappend (byteString "=") (byteString v))) (byteString "\n")) (byteString BS.empty) headers
-requestHeadersToBS headers = toStrict $ toLazyByteString (printableRequestHeaders headers)
+requestHeadersToBS headers = BSL.toStrict $ toLazyByteString (printableRequestHeaders headers)
 getRequestHeader headers hk =
                  let key = C8.pack hk
                  in filter (\(k, v) -> k == key) headers
@@ -116,11 +91,35 @@ findRequestCountryCode headers =
                          -- r <- get $ "http://www.telize.com/geoip/" ++ getIpFromRequest headers
                          return $ r ^. responseBody . key "country_code" . _String
 
+buildServiceData :: Service -> [Item] -> ServiceData
+buildServiceData service datas =
+                 SD { name  = map toLower $ show service
+                    , items = datas
+                    }
+
+--search :: T.Text -> [ServiceData]
+--search query = map getServiceData $ zip dataProviders [query]
+
+--bodyToJSON :: (Body b, Data.ByteString.Lazy.Internal.ByteString lbs) => b -> lbs
+bodyToJSON b = A.encode b
+createResponse :: Body -> BS.ByteString
+createResponse res = BSL.toStrict $ bodyToJSON res
+
 app :: H2.Application
 app = \env ->
-  do
-    --countryCode <- findRequestCountryCode $ H2.httpHeaders env
-    searchResults  <- search $ getQueryParamValue "q" env
-    return $ H2R.set_body_bytestring (encodeUtf8 "") (def { H2.headers = [ ("Content-Type", "text/json") ] })
+    let q = getQueryParamValue "q" env
+        urlComponents = createSpotifyRequest q
+    in do
+        --putStrLn $ "Searching results for term: \"" ++ T.unpack q ++ "\""
+        --countryCode <- findRequestCountryCode $ H2.httpHeaders env
+        r <- getWith (fst urlComponents) (snd urlComponents)
+        --putStrLn "got response from spotify. Parsing..."
+        let results = parseData Spotify r
+        --putStrLn "parsing done...creating response"
+        let searchResults = B $ [buildServiceData Spotify results]
+        let body = createResponse $ searchResults
+        --putStrLn $ C8.unpack body
+        --let searchResults = search $ getQueryParamValue "q" env
+        return $ H2R.set_body_bytestring body (def { H2.headers = [ ("Content-Type", "text/json") ] })
 
 main = run app
