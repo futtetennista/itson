34664e3dc6d31213a64f539c7e002c8965ddde04
Stefano Dacchille <stefano.dacchille@gmail.com>
2015-02-01 01:30:46 +0100

    * Rename all the things * Add check for availability based on request ip

--- Server.hs
+++ Server.hs
@@ -16,57 +16,52 @@ import Data.Text.Encoding (encodeUtf8)
 import qualified Data.ByteString.Char8 as C8 (pack, unpack, split)
 import Data.Monoid
 import qualified Data.ByteString as BS (ByteString(..), empty, unpack)
-import Data.ByteString.Builder
+import qualified Data.ByteString.Builder as BSB
 import qualified Data.ByteString.Lazy as BSL (toStrict)
 import qualified Data.Vector as V (filter, null, empty)
 import qualified Data.Aeson as A
 import Data.Char (toLower)
 
 -- Models
-data Service = Spotify deriving Show
+data DataProvider = Spotify deriving Show
 dataProviders = [Spotify]
 
 data Urls  = U [(T.Text, T.Text)] deriving Show
 data Title = TT T.Text deriving Show
 data Item = I Title Urls | Empty deriving Show
-data ServiceData = SD { name  :: T.Text
-                      , items :: [Item]
-                      } deriving Show
-data Body = B [ServiceData] deriving Show
+data Fact = F { name  :: T.Text
+              , items :: [Item]
+              } deriving Show
+data Result = Res [Fact] deriving Show
 
 instance A.ToJSON Item where
          toJSON (I (TT title) (U kvs)) = A.object $ ("title" A..= title) : ["urls" A..= A.object (map (\(k, v) -> k A..= v) kvs)]
-instance A.ToJSON ServiceData where
-         toJSON (SD name [])    = A.object ["service" A..= name, "items" A..= AT.emptyArray]
-         toJSON (SD name items) = A.object ["service" A..= name, "items" A..= items]
-instance A.ToJSON Body where
-         toJSON (B body) = A.object ["results" A..= body]
+instance A.ToJSON Fact where
+         toJSON (F name [])    = A.object ["service" A..= name, "items" A..= AT.emptyArray]
+         toJSON (F name items) = A.object ["service" A..= name, "items" A..= items]
+instance A.ToJSON Result where
+         toJSON (Res body) = A.object ["results" A..= body]
 
 -- Serialisation
 intercalateM sep xs = T.intercalate sep . filter (/="") $ map (\ m -> case m of { Just v -> v; Nothing -> "" }) xs
---createTitle :: IsString s => Maybe s -> [Maybe s] -> Title 
+createTitle :: Maybe T.Text -> [Maybe T.Text] -> Title
 createTitle (Just t) (a:as) = TT $ t `T.append` " by " `T.append` (intercalateM ", " (a:as))
 createTitle (Just t) []     = TT t
 createTitle Nothing  _      = TT "Song Details Unknown"
---createUrls :: Data.String.IsString s => Maybe s -> Maybe s -> Urls
-createUrls (Just p) (Just f) = U [("preview", p), ("full", f)]
-createUrls Nothing  (Just f) = U [("full", f)]
-createUrls _ _               = U []
 
-createSpotifyRequest query =
-                     (opts, "https://api.spotify.com/v1/search")
-                     where
-                          opts = defaults & param "q"     .~ [query]
-                                          & param "type"  .~ ["track"]
-                                          & param "limit" .~ ["5"]
+createUrls :: Bool -> Maybe T.Text -> Maybe T.Text -> Urls
+createUrls True (Just p) (Just f) = U [("preview", p), ("full", f)]
+createUrls True Nothing  (Just f) = U [("full", f)]
+createUrls False (Just p) _       = U [("preview", p)]
+createUrls False _ _              = U []
+
 -- Parsing
-parseAvailableMarkets r =
-                      let mx = r ^.. responseBody . key "tracks" . key "items" . values . key "available_markets" . _Array
-                      in case mx of
-                              [] -> V.empty
-                              xs -> head mx
-parseData :: (AsValue body) => Service -> Wreq.Response body -> [Item]
-parseData service resp = resp ^.. responseBody . key "tracks" . key "items" . _Array . traverse . to (\o -> I (createTitle (o ^? key "name" . _String) (o ^.. key "artists" . _Array . traverse . to (\a -> a ^? key "name" . _String))) (createUrls (o ^? key "preview_url" . _String) (o ^? key "external_urls" . key "spotify" . _String)))
+parseAvailableMarkets v = v ^. key "available_markets" . _Array . traverse . to (\ms -> ms ^.. _String)
+parseDataProviderResponse :: (AsValue body) => DataProvider -> T.Text -> Wreq.Response body -> [Item]
+parseDataProviderResponse service cc resp =
+          resp ^.. responseBody . key "tracks" . key "items" . _Array . traverse . to (\o -> let trackAvailable = not . null . filter (==cc) $ parseAvailableMarkets o
+        in
+          I (createTitle (o ^? key "name" . _String) (o ^.. key "artists" . _Array . traverse . to (\a -> a ^? key "name" . _String))) (createUrls trackAvailable (o ^? key "preview_url" . _String) (o ^? key "external_urls" . key "spotify" . _String)))
 
 -- Incoming Request
 getQueryParams env =
@@ -74,8 +69,8 @@ getQueryParams env =
                in [ (a, b) | arr <- queryArray, a <- [head arr], b <- tail arr]
 getQueryParamValue p env = snd . head $ filter (\(a, b) -> a == p) (getQueryParams env)
 
-printableRequestHeaders headers = foldr (\(k, v) hx -> mappend (mappend hx $ mappend (byteString k) (mappend (byteString "=") (byteString v))) (byteString "\n")) (byteString BS.empty) headers
-requestHeadersToBS headers = BSL.toStrict $ toLazyByteString (printableRequestHeaders headers)
+--printableRequestHeaders headers = foldr (\(k, v) hx -> mappend (mappend hx $ mappend (byteString k) (mappend (byteString "=") (byteString v))) (byteString "\n")) (byteString BS.empty) headers
+--requestHeadersToBS headers = BSL.toStrict $ toLazyByteString (printableRequestHeaders headers)
 getRequestHeader headers hk =
                  let key = C8.pack hk
                  in filter (\(k, v) -> k == key) headers
@@ -91,33 +86,39 @@ findRequestCountryCode headers =
                          -- r <- get $ "http://www.telize.com/geoip/" ++ getIpFromRequest headers
                          return $ r ^. responseBody . key "country_code" . _String
 
-buildServiceData :: Service -> [Item] -> ServiceData
-buildServiceData service datas =
-                 SD { name  = T.pack . map toLower $ show service
-                    , items = datas
-                    }
+createFact :: DataProvider -> [Item] -> Fact
+createFact dataProvider xs = F { name  = T.pack . map toLower $ show dataProvider
+                                , items = xs
+                                }
 
 --search :: T.Text -> [ServiceData]
 --search query = map getServiceData $ zip dataProviders [query]
 
---bodyToJSON :: (Body b, Data.ByteString.Lazy.Internal.ByteString lbs) => b -> lbs
-bodyToJSON b = A.encode b
-createResponse :: Body -> BS.ByteString
-createResponse res = BSL.toStrict $ bodyToJSON res
+--resultsToJSON :: Response -> Data.ByteString.Lazy
+resultsToJSON b = A.encode b
+bodyByteString :: Result -> BS.ByteString
+bodyByteString res = BSL.toStrict $ resultsToJSON res
+
+createSpotifyRequest searchTerm =
+                     (opts, "https://api.spotify.com/v1/search")
+                     where
+                          opts = defaults & param "q"     .~ [searchTerm]
+                                          & param "type"  .~ ["track"]
+                                          & param "limit" .~ ["5"]
 
 app :: H2.Application
 app = \env ->
-    let q = getQueryParamValue "q" env
+    let q = getQueryParamValue "term" env
         urlComponents = createSpotifyRequest q
     in do
         --putStrLn $ "Searching results for term: \"" ++ T.unpack q ++ "\""
-        --countryCode <- findRequestCountryCode $ H2.httpHeaders env
+        cc <- findRequestCountryCode $ H2.httpHeaders env
         r <- getWith (fst urlComponents) (snd urlComponents)
         --putStrLn "got response from spotify. Parsing..."
-        let results = parseData Spotify r
+        let results = parseDataProviderResponse Spotify cc r
         --putStrLn "parsing done...creating response"
-        let searchResults = B $ [buildServiceData Spotify results]
-        let body = createResponse $ searchResults
+        let searchResults = Res [createFact Spotify results]
+        let body = bodyByteString searchResults
         --putStrLn $ C8.unpack body
         --let searchResults = search $ getQueryParamValue "q" env
         return $ H2R.set_body_bytestring body (def { H2.headers = [ ("Content-Type", "text/json") ] })
